b58acd005cbbe7a2afbf219d5625dde35c2534db
Merthan KARAMAN <merth4nkaraman@gmail.com>
2026-08-26 17:38:50 +0300

    release commit

--- app/src/main/java/com/drivehub/mgha/ui/MainActivity.java
+++ app/src/main/java/com/drivehub/mgha/ui/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
         TextView versionView = findViewById(R.id.text_version);
         versionView.setText(getString(R.string.version_format,
                 BuildConfig.VERSION_NAME,
-                BuildConfig.FLAVOR,
+                BuildConfig.DEBUG ? BuildConfig.FLAVOR : "",
                 BuildConfig.DEBUG ? getString(R.string.version_debug_suffix) : ""));
 
         loadFromSettings();
